cb9a39d1567829bdb21be160c8c51257b9d4f32b
Luís Eduardo <157180909+luiscarlosedu@users.noreply.github.com>
2024-03-23 16:49:40 -0300

    Publicando o site

---
+++ script/app.js
@@ -0,0 +1,56 @@
+// Menu Responsivo
+class MobileNavBar {
+    constructor(mobileMenu, navBar, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navBar);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleMenuToggle = this.handleMenuToggle.bind(this);
+        this.handleLinkClick = this.handleLinkClick.bind(this);
+    }
+
+    animateLinks() {
+        if (this.navList.classList.contains(this.activeClass)) {
+            this.navLinks.forEach((link, index) => {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            });
+        } else {
+            this.navLinks.forEach(link => {
+                link.style.animation = "";
+            });
+        }
+    }
+
+    handleMenuToggle() {
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    handleLinkClick() {
+        this.navList.classList.remove(this.activeClass);
+        this.mobileMenu.classList.remove(this.activeClass);
+    }
+
+    addEventListeners() {
+        this.mobileMenu.addEventListener("click", this.handleMenuToggle);
+        this.navLinks.forEach(link => {
+            link.addEventListener("click", this.handleLinkClick);
+        });
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addEventListeners();
+        }
+        return this;
+    }
+}
+
+const mobileNavbar = new MobileNavBar(
+    ".mobile-menu",
+    ".nav-bar",
+    ".nav-bar a"
+);
+mobileNavbar.init();
